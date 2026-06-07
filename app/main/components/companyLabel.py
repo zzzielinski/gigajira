2da245278c5e22aa29c12757d4d4fdb8b5ee3e01
Szymon Szota <szymonszota@gmail.com>
2026-06-07 20:06:04 +0200

    Added company management menu

---
+++ app/main/components/companyLabel.py
@@ -0,0 +1,36 @@
+import os
+from PySide6.QtCore import QFile, QIODevice, Qt
+from PySide6.QtUiTools import QUiLoader
+
+class CompanyLabel:
+    def __init__(self, id, company):
+        CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+        UI_PATH = os.path.join(CURRENT_DIR, "companyLabel.ui")
+
+        ui_file = QFile(UI_PATH)
+        if not ui_file.open(QIODevice.ReadOnly):
+            raise RuntimeError(f"Cannot open {UI_PATH}")
+
+        loader = QUiLoader()
+        self.window = loader.load(ui_file)
+        ui_file.close()
+
+        self.window.id.setText(id)
+        self.window.company.setText(company)
+
+        self.window.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
+        self.window.setStyleSheet("""
+            #companyLabel {
+                border: 1px solid #cccccc;
+                border-radius: 15px;
+                background-color: #06402b;
+            }
+        """)
+
+        self.window.manageButton.clicked.connect(self.manage)
+
+    def show(self):
+        self.window.show()
+
+    def manage(self):
+        print(f"Manage company {self.window.company.text()}")
