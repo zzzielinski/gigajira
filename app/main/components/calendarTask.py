26ba9c1cdcd8594c7e3dbae3920f72d9dbaf96cc
Szymon Szota <szymonszota@gmail.com>
2026-06-07 21:08:49 +0200

    Added click detection on calendarTask

--- app/main/components/calendarTask.py
+++ app/main/components/calendarTask.py
@@ -1,9 +1,18 @@
 import os
-from PySide6.QtCore import QFile, QIODevice, Qt
+from PySide6.QtCore import QFile, QIODevice, Qt, QObject, Signal, QEvent
 from PySide6.QtUiTools import QUiLoader
 
+class ClickListener(QObject):
+    clicked = Signal()
+
+    def eventFilter(self, watched, event):
+        if event.type() == QEvent.MouseButtonPress and event.button() == Qt.MouseButton.LeftButton:
+            self.clicked.emit()
+            return True
+        return super().eventFilter(watched, event)
+
 class CalendarTask:
-    def __init__(self, title, project, status, priority):
+    def __init__(self, title, project, status, priority, id):
         CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
         UI_PATH = os.path.join(CURRENT_DIR, "calendarTask.ui")
 
@@ -19,6 +28,7 @@ class CalendarTask:
         self.window.project.setText(project)
         self.window.status.setText(status)
         self.window.priority.setText(str(priority))
+        self.id = int(id)
 
         self.window.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
         self.window.setStyleSheet("""
@@ -29,5 +39,12 @@ class CalendarTask:
             }
         """)
 
+        self.clickListener = ClickListener()
+        self.window.installEventFilter(self.clickListener)
+        self.clickListener.clicked.connect(self.manage)
+
     def show(self):
         self.window.show()
+
+    def manage(self):
+        print(f"Manage task {self.id}")
